e0f5ce038e362633ac2d8ee38c94c836891e0a77
Kazem Javadi <kazemjavadi@hotmail.com>
2021-10-17 19:00:00 +0330

    adding PersonOrg model

---
+++ tsetmc.ir/Models/PersonOrg.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tsetmc.ir.Models
+{
+    internal class PersonOrg
+    {
+        //Date of data
+        public string PersianDate { get; set; }
+
+        //Org - Buy
+        //خرید حقوقی
+        public long NumberOfOrgBuyers { get; set; }
+        public long OrgBuyTotalVolume { get; set; }
+        public decimal OrgBuyTotalPrice { get; set; }
+        public decimal OrgBuyAvgSharePrice { get; set; }
+
+        //Person - Buy
+        //خرید حقیقی
+        public long NumberOfPersonBuyers { get; set; }
+        public long PersonBuyTotalVolume { get; set; }
+        public decimal PersonBuyTotalPrice { get; set; }
+        public decimal PersonBuyAvgSharePrice { get; set; }
+
+        //Org - Sell
+        //فروش حقوقی
+        public long NumberOfOrgSellers { get; set; }
+        public long OrgSellTotalVolume { get; set; }
+        public decimal OrgSellTotalPrice { get; set; }
+        public decimal OrgSellAvgSharePrice { get; set; }
+
+        //Person - Sell
+        //فروش حقیقی
+        public long NumberOfPersonSellers { get; set; }
+        public long PersonSellTotalVolume { get; set; }
+        public decimal PersonSellTotalPrice { get; set; }
+        public decimal PersonSellAvgSharePrice { get; set; }
+
+        //Org to Person ownership change
+        //تغییر مالکیت از حقوقی به حقیقی
+        public long OwnershipChangeFromOrgToPerson { get; set; }
+    }
+}
